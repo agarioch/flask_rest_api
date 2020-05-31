c6abb2e7eca43580ce8da72efbf3b92196348111
alistair garioch <alistairgarioch@gmail.com>
2020-05-31 22:43:53 +0100

    replace in memory user list with sqlite db

--- student_search/user.py
+++ student_search/user.py
@@ -1,5 +1,45 @@
+import sqlite3
+
+
 class User:
     def __init__(self, _id, username, password):
         self.id = _id
         self.username = username
         self.password = password
+
+    def __str__(self):
+        return "user (id={}, name={}, pw={})".format(
+            self.id, self.username, self.password
+        )
+
+    @classmethod
+    def find_by_username(cls, username):
+        connection = sqlite3.connect("data.sqlite")
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE username=?"
+        result = cursor.execute(query, (username,))
+        row = result.fetchone()
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+
+        connection.close()
+        return user
+
+    @classmethod
+    def find_by_id(cls, _id):
+        connection = sqlite3.connect("data.sqlite")
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE id=?"
+        result = cursor.execute(query, (_id,))
+        row = result.fetchone()
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+
+        connection.close()
+        return user
