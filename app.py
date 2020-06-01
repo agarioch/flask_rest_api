d24ed26e03493bb067f8fed3f4f516313d8ef3db
alistair garioch <alistairgarioch@gmail.com>
2020-06-01 18:34:02 +0100

    replace hardcoded users list with register api store registered users in database

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
+from user import UserRegister
 
 app = Flask(__name__)
 app.secret_key = "testing"
@@ -69,5 +70,6 @@ class Students(Resource):
 
 api.add_resource(Student, "/student/<string:id>")
 api.add_resource(Students, "/students")
+api.add_resource(UserRegister, "/register")
 
 app.run(port=5000, debug=True)
