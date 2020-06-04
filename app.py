2a2a8d985a312764cc258bf07a9e1e7cad235502
agarioch <alistairgarioch@gmail.com>
2020-06-04 20:34:49 +0100

    replace sql with sql alchemy

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
-from flask_restful import Api
 from flask_jwt import JWT
+from flask_restful import Api
 
-from security import authenticate, identity
-from resources.user import UserRegister
 from resources.student import Student, Students
+from resources.user import UserRegister
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "testing"
 api = Api(app)
 
@@ -15,10 +17,12 @@ api = Api(app)
 # use jwt token to get user
 jst = JWT(app, authenticate, identity)
 
-
 api.add_resource(Student, "/student/<string:id>")
 api.add_resource(Students, "/students")
 api.add_resource(UserRegister, "/register")
 
 if __name__ == "__main__":
+    from db import db
+
+    db.init_app(app)
     app.run(port=5000, debug=True)
--- create_tables.py
+++ create_tables.py
@@ -1,14 +1,12 @@
 import sqlite3
 
-from user import UserRegister
-
 connection = sqlite3.connect("data.sqlite")
 cursor = connection.cursor()
 
 create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
 cursor.execute(create_table)
 
-create_table = "CREATE TABLE IF NOT EXISTS students (id text, name text)"
+create_table = "CREATE TABLE IF NOT EXISTS students (id text PRIMARY KEY, name text)"
 cursor.execute(create_table)
 
 connection.commit()
